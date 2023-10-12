792871eb137681f5b80e1b4949c1e3d5e488d8d0
Casey <leptone55@gmail.com>
2023-10-11 17:17:31 -0700

    prod / dev constants

--- packages/ezkl-lib/src/constants.ts
+++ packages/ezkl-lib/src/constants.ts
@@ -1,6 +1,16 @@
-export const SET_SERVER_DOMAIN = "https://set-membership-server.onrender.com";
-export const PASSPORT_SERVER_DOMAIN =
-  "https://passport-server-rygt.onrender.com";
+const isProd = process.env.NODE_ENV === "production";
+
+export const SET_SERVER_DOMAIN = isProd
+  ? "https://set-membership-server.onrender.com"
+  : "http://127.0.0.1:8000";
+
+export const PASSPORT_SERVER_DOMAIN = isProd
+  ? "https://passport-server-rygt.onrender.com"
+  : "http://localhost:3002";
+
+console.log("PASSPORT_SERVER_DOMAIN", PASSPORT_SERVER_DOMAIN);
+console.log("SET_SERVER_DOMAIN", SET_SERVER_DOMAIN);
+
 export const WASM_PATH = "/ezkl-artifacts/ezkl_bg.wasm";
 export const CHUNK_SIZE = 400; // The max length for each chunk
 export const FRAME_RATE = 200;
