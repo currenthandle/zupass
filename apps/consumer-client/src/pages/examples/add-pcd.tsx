bcbc7acfcf04ff0ff2f87e09a66edb4200dfe191
Casey <leptone55@gmail.com>
2023-10-11 23:47:37 -0700

    remove secret default

--- apps/consumer-client/src/pages/examples/add-pcd.tsx
+++ apps/consumer-client/src/pages/examples/add-pcd.tsx
@@ -312,7 +312,7 @@ async function addEzklSecretProofPCD() {
     {
       secret: {
         argumentType: ArgumentTypeName.String,
-        value: "secret",
+        value: "",
         userProvided: true
       }
     },
